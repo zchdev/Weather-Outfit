33c6782456a7e788c1526597733b7d93b81a511d
zch <zchagra.dev@gmail.com>
2022-11-22 15:32:45 +0100

    Initial Commit

---
+++ Weather Outfit/Weather_OutfitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_OutfitApp.swift
+//  Weather Outfit
+//
+//  Created by zchagra.dev on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Weather_OutfitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
